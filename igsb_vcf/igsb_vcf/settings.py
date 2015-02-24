950c4432e2d1a32d6b58dc2d247ffeff52cf702e
Jason Grundstad <jgrundstad@gmail.com>
2015-02-24 14:57:50 -0600

    created database and my.cnf

--- igsb_vcf/igsb_vcf/settings.py
+++ igsb_vcf/igsb_vcf/settings.py
@@ -59,8 +59,10 @@ WSGI_APPLICATION = 'igsb_vcf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+          'read_default_file': BASE_DIR + '/igsb_vcf/my.cnf',
+        },
     }
 }
 
