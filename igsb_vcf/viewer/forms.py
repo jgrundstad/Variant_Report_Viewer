caa0c89c3807e576b775cb51a34096e110e29790
Jason Grundstad <jgrundstad@gmail.com>
2015-04-06 10:44:00 -0500

    re-ordered fields in forms

--- igsb_vcf/viewer/forms.py
+++ igsb_vcf/viewer/forms.py
@@ -6,11 +6,13 @@ from models import Bnid, Sample, Caller, Vcf, Case
 class BnidForm(forms.ModelForm):
   class Meta:
     model = Bnid
+    fields = ['sample', 'bnid']
 
 
 class SampleForm(forms.ModelForm):
   class Meta:
     model = Sample
+    fields = ['case', 'name']
 
 
 class CallerForm(forms.ModelForm):
@@ -22,6 +24,7 @@ class VcfForm(forms.ModelForm):
   vcf_file = forms.FileField(widget=AdminFileWidget)
   class Meta:
     model = Vcf
+    fields = ['case', 'sample', 'caller', 'vcf_file']
 
 
 class CaseForm(forms.ModelForm):
