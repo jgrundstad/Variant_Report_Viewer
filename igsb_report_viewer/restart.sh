61819fccda4ed284e54033c4edb5d7f4271271c6
Jason Grundstad <jgrundstad@gmail.com>
2015-04-30 15:03:39 -0500

    removed some dev scripts

--- igsb_report_viewer/restart.sh
+++
@@ -1,7 +0,0 @@
-echo mysql login - 
-mysql -uroot -p < restart.sql
-rm -r viewer/migrations
-python manage.py migrate
-echo Create jgrundst superuser - 
-python manage.py createsuperuser --username=jgrundst --email=jgrundstad@gmail.com 
-
